00890014c42a29567bdc1c1e1254f685d8864d0d
CarsonDev1 <buitritinht@gmail.com>
2025-02-14 09:41:07 +0700

    fix deploy

--- src/pages/Admin/PreorderCampaign.jsx
+++ src/pages/Admin/PreorderCampaign.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const PreorderCampaign = () => {
-    return (
-        <div>PreorderCampaign</div>
-    )
-}
+	return <div>PreorderCampaign</div>;
+};
 
-export default PreorderCampaign
+export default PreorderCampaign;
